005a22437ffdd6c9451efa367ff479b5dbb207c3
Karthik Rajan <rajankarthiks@gmail.com>
2016-07-18 17:37:36 +0530

    Write Smart Content

--- resources/views/partials/text-actions.blade.php
+++ resources/views/partials/text-actions.blade.php
@@ -1,5 +1,5 @@
 <div ng-controller="BottomSheet" class="md-padding" ng-cloak>
     <div class="bottom-sheet-demo inset" layout="row" layout-sm="column" layout-align="center">
-        <md-button flex="25" class="md-primary md-raised" ng-click="showGridBottomSheet()">Smart Writing Insights <i class="material-icons" role="presentation">highlight</i></md-button>
+        <md-button flex="25" class="md-primary md-raised" ng-click="showGridBottomSheet()">Smart Writing Insights</md-button>
     </div>
 </div>
