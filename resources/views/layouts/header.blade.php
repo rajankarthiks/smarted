aaa5c1fabe4325dfc898e356ed2b9be5b613019f
Karthik Rajan <rajankarthiks@gmail.com>
2016-07-18 17:04:01 +0530

    Write Smart Content

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 
-    <title>SmartAIEd Ecosystem</title>
+    <title>SmartAIEd</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
